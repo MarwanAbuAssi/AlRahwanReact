5b807dccaa2b95e312fb5d281b73e9f9959685d3
Marwan.Abuassi <marwanabuassi@gmail.com>
2018-07-10 17:05:46 +0300

    Commit * Add Schedule Calender

--- al-rahwan/src/App.js
+++ al-rahwan/src/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+// import FullCalendar from 'fullcalendar';
 
+import 'primereact/resources/themes/omega/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+
+import ScheduleCalender from './Container/Schedule/ScheduleCalender'
 import './App.css';
 import Login from './Container/Login/Login'
 class App extends Component {
   render() {
     return (
       <div className="">
-          <Login/>
+          {/*<Login/>*/}
+        <ScheduleCalender/>
 
       </div>
     );
